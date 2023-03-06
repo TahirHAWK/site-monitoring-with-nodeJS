91f3164f837da7321a2683801f667fcddcf03223
TahirHSTU <tahirtamin20@gmail.com>
2023-03-06 21:31:32 +0600

    added some modifications

--- model/fileModifier.js
+++ model/fileModifier.js
@@ -36,22 +36,26 @@ FileModifier.prototype.insertNewWebPageAndCompare = function(pageLink, scrapedWe
           console.log("// it means no change has occured")
           resolve()
         } else{
-          console.log("// here it means a change has detected, and now it should be replaced with the old one and update the time of change detection, make the property of telegram status to 'sendReady'. change the telegram notification status to 'sent' after the cronjob has done(cronjob function has to be made in such a way that it modifies the telegram status of that specific data) ")
-          siteData.findOneAndUpdate({pageLink: pageLink}, {WebPage: scrapedWebPage, telegramStatus: 'sendReady'})
+          console.log("// here it means a change has detected, and now it should be replaced with the old one and update the time of change detection, make the property of telegram status to 'sendReady'. change the telegram notification status to 'sent' after the cronjob has done(cronjob function has to be made in such a way that it modifies the telegram status of that specific data)")
+          siteData.updateOne({pageLink: pageLink}, {WebPage: scrapedWebPage, telegramStatus: 'sendReady'}).then((confirmation)=>{
+            console.log(confirmation, '// New change on the site has been updated for telegram.')
+            resolve()
+          })
           
-          resolve()
         }
       
-    }).catch((diffResult)=>{
+    }).catch((diffResult)=> {
       console.log("// 3. if not, then store it for the first time for future comparison")
-      siteData.insertOne({pageLink: pageLink, WebPage: scrapedWebPage, telegramStatus: 'new', }).then((res)=>{
+      siteData.insertOne({pageLink: pageLink, WebPage: scrapedWebPage, telegramStatus: 'new' }).then((res)=>{
         resolve("added new page to db")
       }).catch((err)=>{
         this.errors.push("Couldn't store the new data.")
         reject("couldn't add the new data to db")
       })
-    })
-  })
+    }) // end of first .then block
+
+
+  }) // end of promise
 }
 
 module.exports = FileModifier
